a2e9194c6142131ed45a452b1c671e4ae29071fb
Dell <Dell@DESKTOP-MH30050>
2026-01-22 10:56:35 +0530

    Plese Push It

---
+++ dummy-crm-automation/src/test/java/demo/FirstWork.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class FirstWork {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This Is my Fierst Program");
+	}
+
+}
